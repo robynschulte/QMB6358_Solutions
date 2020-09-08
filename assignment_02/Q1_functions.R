86a04b61408608ba87c6f00e1cd2990212e3f326
robynschulte <70485914+robynschulte@users.noreply.github.com>
2020-09-08 18:22:33 -0400

    Create Q1_functions.R

---
+++ assignment_02/Q1_functions.R
@@ -0,0 +1,63 @@
+
+################################################################################
+#
+# QMB 6358: Software Tools for Business Analytics
+# Library of R Functions for Assignment 2
+#
+# Name: Robyn Schulte
+# College of Business Administration
+# University of Central Florida
+#
+# Date: 09/08/2020
+#
+################################################################################
+#
+# This program is a library of R functions for Assignment 2.
+#
+#
+#
+################################################################################
+
+
+
+################################################################################
+# Load Packages
+################################################################################
+
+# Load any packages that are needed for these functions.
+# library(package_name_goes_here)
+
+# You would typically install the package first,
+# instead of within the library.
+# install.packages("package_name_goes_here")
+
+
+################################################################################
+# Functions
+################################################################################
+
+#--------------------------------------------------------------------------------
+# First function: Sum of squared deviations
+#--------------------------------------------------------------------------------
+
+# Function definition goes here.
+x <- c(1,2,3,4)
+
+sum_sq_dev <- sum( (x-mean(x) )^2)
+          
+print(sum_sq_dev)
+
+#--------------------------------------------------------------------------------
+# Second function: Covariance between two variables
+#--------------------------------------------------------------------------------
+
+# Function definition goes here.
+
+
+
+
+
+################################################################################
+# End
+################################################################################
+
