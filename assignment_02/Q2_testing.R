8024379569bc2eed8b1801cd46a6cc3ccf549485
Lee Morin <42477710+LeeMorinUCF@users.noreply.github.com>
2020-09-10 14:00:45 -0400

    Comments on halfway checkpoint for A2 Q2

--- assignment_02/Q2_testing.R
+++ assignment_02/Q2_testing.R
@@ -1,3 +1,10 @@
+# Please see the revised testing script Q2_testing.R
+# Aguments of covar_y_x should be (y, x)
+
+# Next step: 
+# Print both the return value and the test against the expected response
+# for both of your examples.
+# And make sure that the test values are correct.
 
 ################################################################################
 #
@@ -52,7 +59,10 @@ print("Testing function sum_sq_dev:")
 # Check values that are output.
 print(sum_sq_dev(x = c(1, 2, 3), mu = 4))
 # Test output against expected answer.
+# This is not the correct answer:
 print(sum_sq_dev(x = c(2, 4, 6), mu = 8) == 0)
+# It should not evaluate to zero. 
+# Work it out on paper to find the answer you should get. 
 
 # Add more examples.
 
@@ -66,6 +76,7 @@ print("Testing function covar_y_x:")
 print(covar_y_x(x = c(1, 2, 3), mu = c(4, 5, 6)))
 # Test output against expected answer.
 print(covar_y_x(x = c(2, 4, 8), mu = c(1, 3, 5)) == 0)
+# Again, zero is not the result you would expect for these inputs.
 
 # Add more examples.
 
