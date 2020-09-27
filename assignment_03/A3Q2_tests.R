859ec74d2b670c62055c8db2bf7b484f82cae39e
robynschulte <70485914+robynschulte@users.noreply.github.com>
2020-09-27 18:07:06 -0400

    Update A3Q2_tests.R

--- assignment_03/A3Q2_tests.R
+++ assignment_03/A3Q2_tests.R
@@ -4,11 +4,11 @@
 # QMB 6358: Software Tools for Business Analytics
 # Assignment 3: Data Handling
 #
-# Name:
+# Name: Robyn Schulte
 # College of Business
 # University of Central Florida
 #
-# Date:
+# Date: 09/27/2020
 #
 ################################################################################
 #
@@ -33,12 +33,12 @@ rm(list=ls(all=TRUE))
 
 # Set working directory to the path containing the csv files.
 # wd_path <- "/path/to/your/folder
-# wd_path <- "C:/Users/le279259/Documents/Teaching/QMB6358_Fall_2020/Assignments/assignment_03/A3_iris_data/QMB6358-iris"
-# setwd(wd_path)
+wd_path <- "C:/Users/robyn/OneDrive/Documents/QMB6358/My assignments1/QMB6358_Solutions/assignment_03"
+setwd(wd_path)
 
 
 # Set paths to other folders.
-# data_path <- sprintf('%s/data', wd_path)
+data_path <- sprintf(A3Q1_data, wd_path)
 # Optional: Organize data in a separate folder for larger projects.
 
 
@@ -51,16 +51,40 @@ print("Contents of Dataset for Question 2a):")
 
 # Read in the dataset.
 
+A3Q2a_full <-read.table(A3Q2a_full.csv)
+
 # Copy the commands from Question 1 to print out results.
 
+print("Dimensions of dataset:")
+print(dim(A3Q2a_full))
+
+if (!is.null(A3Q2a_full) && ncol(A3Q2a_full) == 6) {
+  colnames(A3Q2a_full) <- c("Sample", "Sepal.Length", "Sepal.Width",
+                           "Petal.Length", "Petal.Width",  "Species")
+}
+print(summary(A3Q2a_full))
+
+print(table(A3Q2a_full[, 6], useNA = 'ifany'))
 
 print("Contents of Dataset for Question 2b):")
 
 # Read in the dataset.
 
+A3Q2b_full <- read.table(A3Q2b_full.csv)
+
 # Copy the commands from Question 1 to print out results.
 
+print("Dimensions of dataset:")
+print(dim(A3Q2b_full))
 
+
+if (!is.null(A3Q2b_full) && ncol(A3Q2b_full) == 6) {
+  colnames(A3Q2b_full) <- c("Sample", "Sepal.Length", "Sepal.Width",
+                           "Petal.Length", "Petal.Width",  "Species")
+}
+print(summary(A3Q2b_full))
+
+print(table(A3Q2b_full[, 6], useNA = 'ifany'))
 
 
 ################################################################################
