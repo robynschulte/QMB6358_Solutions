c2bffab2a3aeae658df3fe96bf692ab5948f97b0
robynschulte <70485914+robynschulte@users.noreply.github.com>
2020-12-10 18:47:34 -0800

    Add files via upload

---
+++ assignment_9/assignment_09.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+##############
+# Final Exam Shell Script
+# Robyn Schulte
+##############
+
+# Navigate to working directory
+cd C:\Users\robyn\OneDrive\Documents\QMB6358\My assignments2\QMB6358_Solutions\assignment_09
+
+# Redirect output
+Rscript Final_Exam.R > Final_Exam_Output.out
+
